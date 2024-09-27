88155fb05af72258bbf3d5da58ced70f62b687d9
drewmercer-bl <drew.mercer@brandlive.com>
2024-09-27 08:04:41 -0700

    refactor(main.tf): changing output names, fixing aud attribute

--- output.tf
+++ output.tf
@@ -1,19 +1,9 @@
-output "google_service_account" {
-  description = "Service Account used by GitHub integration"
-  value       = google_service_account.github.name
-}
-
-output "service_account_github_email" {
-  description = "Service Account Email used by GitHub integration"
-  value       = google_service_account.github.email
-}
-
-output "service_account_github_id" {
+output "WIF_SERVICE_ACCOUNT" {
   description = "Service Account ID used by GitHub integration"
   value       = google_service_account.github.id
 }
 
-output "google_iam_workload_identity_pool" {
+output "WIF_PROVIDER" {
   description = "Workload Identity Pool Provider Name"
-  value       = google_iam_workload_identity_pool.github.name
+  value       = "${google_iam_workload_identity_pool.github.name}/providers/github-provider"
 }
