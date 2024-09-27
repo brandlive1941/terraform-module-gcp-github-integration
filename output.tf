6b5e7e679cfde82242a49b39f70afbde9ce85136
drewmercer-bl <drew.mercer@brandlive.com>
2024-09-27 09:08:05 -0700

    more output fixes

--- output.tf
+++ output.tf
@@ -3,6 +3,11 @@ output "service_account_id" {
   value       = google_service_account.github.id
 }
 
+output "service_account_email" {
+  description = "Service Account Email used by GitHub integration"
+  value       = google_service_account.github.email
+}
+
 output "workload_identity_pool_provider_id" {
   description = "Workload Identity Pool Provider ID"
   value       = "${google_iam_workload_identity_pool.github.name}/providers/github-provider"
